ed12913f13133e71ad17aa7b64e893b14609cea5
jambion <ambion_j@yahoo.com>
2017-09-05 09:39:00 -0700

    Added email regex and flash for errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
-  validates :user_email, presence: true 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates(:user_email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false })
+  validates :rating, presence: true
 end
