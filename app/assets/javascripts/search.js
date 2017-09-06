500581a79a2442f22ed4ddb8030f4400f46d58cd
jambion <ambion_j@yahoo.com>
2017-09-05 20:49:59 -0700

    Refactor code

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,8 +4,6 @@ $(document).ready(function () {
   99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 
   27: "Horror", 10402: "Music", 9648: "Mystery", 10479: "Romance", 
   878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western" }
-  
-  var newTableObject = $(".search-table");
 
   var $search_list = $("#search-list");
   
